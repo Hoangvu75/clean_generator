4ff4c506329ad6a9b984178b292cd55fb9789597
Vu Huy Hoang <uchihavuhuyhoang@gmail.com>
2023-12-02 21:32:09 +0700

    initialize

---
+++ bin/clean_generator.dart
@@ -0,0 +1,5 @@
+import 'package:clean_generator/clean_generator.dart' as clean_generator;
+
+void main(List<String> arguments) {
+  print('Hello world: ${clean_generator.calculate()}!');
+}
---
+++ test/clean_generator_test.dart
@@ -0,0 +1,8 @@
+import 'package:clean_generator/clean_generator.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
